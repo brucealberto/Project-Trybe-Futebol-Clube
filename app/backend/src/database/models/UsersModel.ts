f1d39be1b596a62f61e81d1a72aa6435aadc93e2
brucealberto <brucealberto2@gmail.com>
2022-07-11 00:31:33 -0300

    feat users-migration

--- app/backend/src/database/models/UsersModel.ts
+++ app/backend/src/database/models/UsersModel.ts
@@ -1,7 +1,29 @@
-import { Model } from 'sequelize';
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
 
 class Users extends Model {
-
+  public id: number;
+  public username: string;
+  public role: string;
+  public email: string;
+  public password: string;
 }
+
+Users.init(
+  {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    username: DataTypes.STRING,
+    role: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+  },
+  {
+    // ... Outras configs
+    underscored: true,
+    sequelize: db,
+    // modelName: 'example',
+    timestamps: false,
+  },
+);
 
 export default Users;
