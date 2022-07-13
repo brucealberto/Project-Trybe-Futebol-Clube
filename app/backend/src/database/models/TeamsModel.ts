cce836b5b58b7ff6c5f8807907c26f347e779d9c
brucealberto <brucealberto2@gmail.com>
2022-07-13 11:48:55 -0300

    feat: teamsModel

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,20 @@
+import { DataTypes, Model } from 'sequelize/types';
+import db from '.';
+
+export default class Teams extends Model {
+  public id: number;
+  public teamName: string;
+}
+
+Teams.init(
+  {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    teamName: DataTypes.STRING,
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'Teams',
+    timestamps: false,
+  },
+);
