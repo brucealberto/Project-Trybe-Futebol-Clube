58176d1ec288aff198bba883e031752737f7422c
brucealberto <brucealberto2@gmail.com>
2022-07-12 21:44:51 -0300

    teste

--- app/backend/src/database/services/login-service.ts
+++ app/backend/src/database/services/login-service.ts
@@ -1,7 +1,7 @@
 import { IService } from '../interfaces';
 import Users from '../models/UsersModel';
 import repositoryUsers from '../repository/repositoryUsers';
-import generateToken from '../utils/generateToken';
+// import generateToken from '../utils/generateToken';
 
 export default class LoginService implements IService {
   constructor(private repository: repositoryUsers) {
@@ -12,14 +12,12 @@ export default class LoginService implements IService {
     if (!data.email || !data.password) {
       throw new Error('All fields must be filled');
     }
-    if (!data.email.includes('@') || !data.email.includes('.com')) {
-      throw new Error('Incorrect email or password');
-    }
+
     if (data.password.length <= 6) {
       throw new Error('Incorrect email or password');
     }
     const login = await this.repository.find({ where: { email: data.email } });
-    generateToken(login);
+    // generateToken(login);
     return login;
   }
 
@@ -30,6 +28,9 @@ export default class LoginService implements IService {
 }
 
 /**
+ *    if (!data.email.includes('@') || !data.email.includes('.com')) {
+      throw new Error('Incorrect email or password');
+    }
  * const hashedPassword = await bcrypt.hash(req.body.password, 10)
  * const user = {name : req.body, password: hashedPassword}
  */
