619381f2457529b42098b504883a9266b9d900eb
brucealberto <brucealberto2@gmail.com>
2022-07-14 10:56:43 -0300

    feat: relationship Matches with Teams

--- app/backend/src/database/models/MatchesModel.ts
+++ app/backend/src/database/models/MatchesModel.ts
@@ -27,11 +27,23 @@ Matches.init(
     timestamps: false,
   },
 );
+
 Matches.belongsTo(Teams, {
   foreignKey: 'homeTeam',
   as: 'teamHome',
 });
+
 Matches.belongsTo(Teams, {
+  foreignKey: 'awayTeam',
+  as: 'teamAway',
+});
+
+Teams.hasMany(Matches, {
+  foreignKey: 'homeTeam',
+  as: 'teamHome',
+});
+
+Teams.hasMany(Matches, {
   foreignKey: 'awayTeam',
   as: 'teamAway',
 });
--- app/backend/src/database/repository/repositoryMatches.ts
+++ app/backend/src/database/repository/repositoryMatches.ts
@@ -1,4 +1,5 @@
 import Matches from '../models/MatchesModel';
+import Teams from '../models/TeamsModel';
 
 export default class RepositoryMatches {
   constructor(private matchesModel = Matches) {
@@ -6,7 +7,16 @@ export default class RepositoryMatches {
   }
 
   async list():Promise<Matches[]> {
-    const result = await this.matchesModel.findAll();
+    const result = await this.matchesModel.findAll({
+      include: [{
+        model: Teams,
+        as: 'teamHome',
+        attributes: { exclude: ['id'] } },
+      {
+        model: Teams,
+        as: 'teamAway',
+        attributes: { exclude: ['id'] },
+      }] });
     return result;
   }
 }
