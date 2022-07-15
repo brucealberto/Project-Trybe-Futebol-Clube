d116f4860e5cb06c96c817a826ac870017a5e683
brucealberto <brucealberto2@gmail.com>
2022-07-15 09:20:10 -0300

    test

--- app/backend/src/database/services/matches-service.ts
+++ app/backend/src/database/services/matches-service.ts
@@ -11,14 +11,21 @@ export default class MatchesService {
     return result;
   }
 
-  async create(data:MatchesModel): Promise<MatchesModel> {
-    const find = await this.repository.findTeamsByPk(data.id);
-    if (!find) { throw new Error('There is no team with such id!'); }
-    if (data.homeTeam === data.awayTeam) {
-      throw new Error('It is not possible to create a match with two equal teams');
+  async create(data:MatchesModel) {
+    try {
+      if (data.homeTeam === data.awayTeam) {
+        throw new Error('It is not possible to create a match with two equal teams');
+      }
+
+      const find = await this.repository.findTeamsByPk(data.id);
+
+      if (!find) { throw new Error('There is no team with such id!'); }
+
+      const result = await this.repository.create(data);
+      return result;
+    } catch (error) {
+      console.log(error);
     }
-    const result = await this.repository.create(data);
-    return result;
   }
 
   // async findByPk(id:number) : Promise<MatchesModel> {
